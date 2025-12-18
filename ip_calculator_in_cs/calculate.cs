a51d9b3db5035e082ce9d5853ad48b450ba0f370
ptky <danielpataky2@gmail.com>
2025-12-18 10:08:17 +0100

    kesz finally

---
+++ ip_calculator_in_cs/calculate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculate_Data
+{
+    class Calculate
+    {
+        public static List<int> szamok = new List<int> { 4, 8, 16, 32, 64, 128, 256, 512 };
+        public static List<int> closest_num = new List<int>();
+        public static List<int> masks = new List<int>();
+        public static void sort_lists(string[] ip, List<int> hosts, List<int> netw_index)
+        {
+            closest_num.Clear();
+            masks.Clear();
+            for (int i = 0, w = hosts.Count; i < w; i++)
+            {
+                foreach (int j in szamok)
+                {
+                    if (j >= hosts[i] + 2)
+                    {
+                        closest_num.Add(j);
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            calculate_mask();
+            calculate_everything(ip, netw_index);
+
+        }
+
+        private static void calculate_mask()
+        {
+            int[] masks_list = { 0, 128, 192, 224, 240, 248, 252, 254, 255 };
+            for (int i = 0, w = closest_num.Count; i < w; i++)
+            {
+                int hostbits = (int)Math.Round(Math.Log2(closest_num[i]));
+                int prefix_l = 32 - hostbits;
+                int index = prefix_l - 24;
+                int choosen = masks_list[index];
+                masks.Add(choosen);
+            }
+
+        }
+        public static void calculate_everything(string[] ip, List<int> netw_index)
+        {
+            int start_ip = int.Parse(ip[3]);
+            int third_ip = int.Parse(ip[2]);
+            string design = "----------------------------------------";
+            Console.WriteLine();
+            File.WriteAllText("ipcalculation.txt", "Made by Pataky\n\n");
+            for (int i = 0, w = closest_num.Count; i < w; i++)
+            {
+                string data = $"{design}\n{netw_index[i]}. network\nClosest Number to hosts: {closest_num[i]}\nIP Address = 192.168.{third_ip}.{start_ip}\n\nMask = 255.255.255.{masks[i]}\nFirst Usable = 192.168.{third_ip}.{start_ip + 1} \nGateway = 192.168.{third_ip}.{start_ip + closest_num[i] - 2} \nBroadcast = 192.168.{third_ip}.{start_ip + closest_num[i] - 1} \n";
+                File.AppendAllText("ipcalculation.txt", data);
+                start_ip += closest_num[i];
+            }
+            File.AppendAllText("ipcalculation.txt", design);
+            Console.Clear();
+            Console.WriteLine("All calculations have been saved to 'ipcalculation.txt'.\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ ip_calculator_in_cs/main.cs
@@ -0,0 +1,83 @@
+﻿using Calculate_Data;
+namespace Ip_Calculator;
+
+class Get_info
+{
+    public static void Main()
+    {
+        int network_c = network_count();
+        var (ip, hosts, network_index) = ip_and_hosts(network_c);
+        Calculate.sort_lists(ip, hosts, network_index);
+    }
+
+    public static (string[] ip, List<int> hostok, List<int> netw_index) ip_and_hosts(int netw)
+    {
+        List<int> ip_address = new List<int>();
+        Console.Clear();
+        Console.Write("Enter IP Address: ");
+        string ip = Console.ReadLine();
+        string[] ip_splitted = ip.Split(".");
+        while (ip_splitted.Length < 4)
+        {
+            Console.WriteLine("Invalid IP\n");
+            Console.Write("Enter IP Address: ");
+            ip = Console.ReadLine();
+            ip_splitted = new string[0];
+            ip_splitted = ip.Split(".");
+        }
+        List<int> hostok = new List<int>(netw);
+        List<int> networkIndex = new List<int>();
+        for (int i = 0; i < netw; i++)
+        {
+            Console.Clear();
+            Console.Write($"Enter the amount of hosts you want to fit in in network {i}.: ");
+            int mennyiseg = int.Parse(Console.ReadLine());
+            hostok.Add(mennyiseg);
+            networkIndex.Add(i);
+        }
+        for (int i = 0, y = hostok.Count; i < y - 1; i++)
+        {
+            for (int j = i + 1, w = hostok.Count; j < w; j++)
+            {
+                if (hostok[i] < hostok[j])
+                {
+                    (hostok[i], hostok[j]) = (hostok[j], hostok[i]);
+                    (networkIndex[i], networkIndex[j]) = (networkIndex[j], networkIndex[i]);
+                }
+            }
+        }
+        return (ip_splitted, hostok, networkIndex);
+    }
+
+
+    public static int network_count()
+    {
+        Console.Write("Enter the amount of networks: ");
+        try
+        {
+            int amount = int.Parse(Console.ReadLine());
+            return amount;
+        }
+        catch
+        {
+            int amount = 0;
+            while (amount <= 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid number \n");
+                Console.Write("Enter the amount of networks: ");
+                try
+                {
+                    amount = int.Parse(Console.ReadLine());
+                    return amount;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+        return 0;
+
+    }
+}
